e36197b5e4155db1b96c3ac8c03f60eb768a3d01
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-20 21:17:35 +0000

    updating

--- exercises/08-Delete_element/app.py
+++ exercises/08-Delete_element/app.py
@@ -3,6 +3,13 @@ people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
 #Your code go here:
 def deletePerson(person_name):
     #Your code go here:
+    for i in people:
+        if i == person_name:
+            people.remove(i)
+            return people
+        else:
+            return people
+
     
 print(deletePerson("daniella"))
 print(deletePerson("juan"))
