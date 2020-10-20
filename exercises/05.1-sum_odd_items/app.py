deb3db558571b1eec622b438d8d4ce0cd2eacdd4
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-20 00:58:50 +0000

    updating

--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -2,4 +2,11 @@ arr = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 #Your code go here:
 
+def sumOdds():
+    value = 0
+    for number in arr:
+        if number % 2 == 1:
+            value = number + value
+            print(value)
 
+sumOdds()
