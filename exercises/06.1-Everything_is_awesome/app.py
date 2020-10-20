cfe094d9ff487575ec30c9c04c4062467a1b8418
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-20 20:22:58 +0000

    updating

--- exercises/06.1-Everything_is_awesome/app.py
+++ exercises/06.1-Everything_is_awesome/app.py
@@ -2,9 +2,13 @@ my_list = [ 1, 0, 0, 0, 1, 0, 0, 0, 1, 1 ]
 
 def my_function(numbers):
     new_list = []
-    for numb in numbers:
+    for i in my_list:
     #The magic go here:
-        
+        if i == 1:
+            new_list.append(1)
+        else:
+            new_list.append("Yahoo")
+
     return new_list
 print(my_function(my_list))
 
