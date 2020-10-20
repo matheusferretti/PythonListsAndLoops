f26633f03e71a521f6a27d177585fa714fe38583
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-20 00:41:24 +0000

    updating

--- exercises/05-Sum_all_items/app.py
+++ exercises/05-Sum_all_items/app.py
@@ -5,7 +5,9 @@ def sum_all_values(items):
 
     total= 0
     #The magic happens here:
-   
+    for x in my_sample_list:
+        total += x
 
     return total
+
 print(sum_all_values(my_sample_list))
