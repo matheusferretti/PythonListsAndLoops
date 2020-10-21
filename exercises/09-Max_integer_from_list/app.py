acdd0b04aeb63ebb03eb33b98c8c08f163b43b11
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-21 23:07:05 +0000

    updating

--- exercises/09-Max_integer_from_list/app.py
+++ exercises/09-Max_integer_from_list/app.py
@@ -3,3 +3,11 @@ my_list = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34]
 
 #Your code go from here:
 
+def biggest_integer(listing):
+    max_value = 0
+    for x in my_list:
+        if x > max_value:
+            max_value = x
+    return max_value
+
+print(biggest_integer(my_list))
