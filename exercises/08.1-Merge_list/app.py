e5756fca25d31ca1d4959eef2cf7fc3371620cb4
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-21 22:42:14 +0000

    updating

--- exercises/08.1-Merge_list/app.py
+++ exercises/08.1-Merge_list/app.py
@@ -4,5 +4,12 @@ chunk_two = [ 'Lucas' , 'Jake','Scott','Amy', 'Molly','Hannah','Lucas']
 
 def merge_list(list1, list2):
     #Your code go here:
-    
+    new_list = []
+    for x in chunk_one:
+        new_list.append(x)
+
+    for z in chunk_two:
+        new_list.append(z)
+    return new_list
+
 print(merge_list(chunk_one, chunk_two))
