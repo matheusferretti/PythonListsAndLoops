97b8bdc9f5412c70bf50de48470ee94923cc6a74
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-21 23:13:29 +0000

    updating

--- exercises/09.1-For_loop_min_value/app.py
+++ exercises/09.1-For_loop_min_value/app.py
@@ -5,3 +5,12 @@ my_list = [3344,34334,454543,342534,4563456,3445,23455,234,262,2335,
 35,243,371,3251,351223,13231243,734,856,56,53,234342,56,545343]
 
 #Your code here:
+
+def smallest_integer(listing):
+    my_array = 1000000000000
+    for x in listing:
+        if x < my_array:
+            my_array = x
+    return my_array
+
+print(smallest_integer(my_list))
