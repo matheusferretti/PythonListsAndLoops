3e2879b75941ab2e72ef7b3c596c275d946be6c5
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-21 23:38:03 +0000

    updating

--- exercises/10-Find_avg/app.py
+++ exercises/10-Find_avg/app.py
@@ -2,3 +2,10 @@ my_list = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5
 
 #Your code here:
 
+def average(items):
+    value = 0
+    for x in items:
+        value += x
+    return value/len(items)
+
+print(average(my_list))
