ed1e89512f88d4fc4c86758ed1f831135b894827
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-22 00:53:56 +0000

    updating

--- exercises/12.5-Yes_and_no/app.py
+++ exercises/12.5-Yes_and_no/app.py
@@ -1,5 +1,11 @@
 theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
 
 #Your code go here:
+def replaceNumber(x):
+    if x == 1:
+        return "wiki"
+    else:
+        return "woko"
 
-
+mapping = list(map(replaceNumber , theBools))
+print(mapping)
