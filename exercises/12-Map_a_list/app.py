4a544252421b3680d4cb18bbb72bf5f74555eab5
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-22 00:20:37 +0000

    updating

--- exercises/12-Map_a_list/app.py
+++ exercises/12-Map_a_list/app.py
@@ -2,8 +2,10 @@ Celsius_values = [-2,34,56,-10]
 
 
 
-def fahrenheit_values(x):
+def fahrenheit_values(value):
 # the magic go here:
-   
-result = list(map(fahrenheit_values, Celsius_values))
+    fahrenheit = (value * 9/5) + 32
+    return fahrenheit
+result = map(fahrenheit_values, Celsius_values)
+result = list(result)
 print(result)
