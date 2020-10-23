3a69a5bf3da97262c1a26e0901df8d2a0cae4b4b
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-23 23:29:24 +0000

    updating

--- exercises/14-Loop-dictionary/app.py
+++ exercises/14-Loop-dictionary/app.py
@@ -1,8 +1,18 @@
 
 spanish_translations = { "dog": "perro", "house": "casa", "cat": "gato" }
 
+# wrong approach below(incomplete)
+for i in range(3):
+    spanish_translations["love"] = "amor"
+    spanish_translations["code"] = "codigo"
+    spanish_translations["smart"] = "inteligente"
 
+print(spanish_translations)
 
-print("Translation", spanish_translations["dog"])
-print("All", spanish_translations)
+# for i in range(3):
+#     print(key)
+#     print(spanish_translations[key])
+
+# print("Translation", spanish_translations["dog"])
+# print("All", spanish_translations)
 
