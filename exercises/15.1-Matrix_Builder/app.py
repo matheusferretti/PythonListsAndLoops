d265995bf446200c12cef26896449ea7947134ae
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-24 00:55:21 +0000

    updating

--- exercises/15.1-Matrix_Builder/app.py
+++ exercises/15.1-Matrix_Builder/app.py
@@ -1,3 +1,12 @@
 #Import random
 
 #Create the function below:
+def matrixBuilder(integer):
+    list1 = []
+    for i in range(integer):
+        list1.append([])
+        for x in range(integer):
+            list1[i].append(1)
+    print(list1)
+
+matrixBuilder(5)
