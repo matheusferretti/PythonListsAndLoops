1eee5e7c44fe0f9689823cb6d6854b2c21e79c58
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-24 00:37:31 +0000

    updating

--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,4 +1,13 @@
-
+def lyrics_generator(list1):
+    lyrics = ""
+    for i in range(len(list1)):
+        if list1[i] == 1 and list1[i-1] == 1 and list1[i-2] == 1:
+            lyrics += "Drop the base !!!Break the base!!!"
+        elif list1[i] == 0:
+            lyrics += "Boom"
+        else:
+            lyrics += "Drop the base"
+    return lyrics
 
 # Your code go above, nothing to change after this line:
 print(lyrics_generator([0,0,1,1,0,0,0]))
